9e10ab484215781d0b7b89f09b833dae00c0c27a
CalinCretu <calincretu1412@gmail.com>
2024-05-14 14:25:26 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Models/PizzasViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class PizzasViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+
+        public PizzasViewModel() { }
+
+        public PizzasViewModel(string name, string description, string immage, double price)
+        {
+            Name = name;
+            Description = description;
+            Image = immage;
+            Price = price;
+        }
+    }
+}
